5ab637557fb2c11992e5d5a597325052071e5b4a
Nazar <nazar.yezerskyi@gmail.com>
2025-01-24 13:54:01 +0200

    feat: added promo-codes module

---
+++ src/promo-codes/promo-codes.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { PromoCodesController } from './promo-codes.controller';
+import { PromoCodesService } from './promo-codes.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { CompanyModule } from 'src/company/company.module';
+import { EventsModule } from 'src/events/events.module';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [PrismaModule, CompanyModule, EventsModule, JwtModule],
+  controllers: [PromoCodesController],
+  providers: [PromoCodesService]
+})
+export class PromoCodesModule {}
